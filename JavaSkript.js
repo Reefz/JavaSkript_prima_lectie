287e93ba9126a4376c8bf79c6bf61eccc74fcc1d
Reefz <catalin112511@gmail.com>
2022-10-20 20:29:53 +0300

    first commit

---
+++ JavaSkript.js
@@ -0,0 +1,33 @@
+// const name = 1;
+// let name1 = "string";
+// let name2 = true;
+// let name3 = false;
+// let name4 = null;
+// let name5 = undefined;
+// // var name2 = 3;
+
+
+
+
+// console.log(name);
+// console.log(name1);
+
+
+
+// let score = null;
+
+// let ion = 2;
+// let vasea = 4;
+
+// score = ion + vasea
+
+//  const nume = "Craciun"
+//  const prenume = "Catalin"
+
+// console.log(nume + "  " + prenume);
+// // console.log(score);
+
+
+alert("sall")
+prompt("salut eu sunt Catalin")
+prompt("citi ani ai?")
