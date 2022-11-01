9acf7132d46fdfeb3d2956f85380fa93805b49c5
Reefz <catalin112511@gmail.com>
2022-11-01 20:36:44 +0200

    yey

--- JavaSkript.js
+++ JavaSkript.js
@@ -1,33 +1,127 @@
-// const name = 1;
-// let name1 = "string";
-// let name2 = true;
-// let name3 = false;
-// let name4 = null;
-// let name5 = undefined;
-// // var name2 = 3;
+// // const name = 1;
+// // let name1 = "string";
+// // let name2 = true;
+// // let name3 = false;
+// // let name4 = null;
+// // let name5 = undefined;
+// // // var name2 = 3;
 
 
 
 
-// console.log(name);
-// console.log(name1);
+// // console.log(name);
+// // console.log(name1);
 
 
 
-// let score = null;
+// // let score = null;
 
-// let ion = 2;
-// let vasea = 4;
+// // let ion = 2;
+// // let vasea = 4;
 
-// score = ion + vasea
+// // score = ion + vasea
 
-//  const nume = "Craciun"
-//  const prenume = "Catalin"
+// //  const nume = "Craciun"
+// //  const prenume = "Catalin"
 
-// console.log(nume + "  " + prenume);
-// // console.log(score);
+// // console.log(nume + "  " + prenume);
+// // // console.log(score);
 
 
-alert("sall")
-prompt("salut eu sunt Catalin")
-prompt("citi ani ai?")
+// // alert("sall")
+// // prompt("salut eu sunt Catalin")
+// // prompt("citi ani ai?")
+
+// let namber = 5;
+// let zece = 10;
+// console.log(zece);
+// zece = 50;
+// let score = namber + zece
+// console.log(zece);
+
+// console.log(score);
+
+// const a= 2;
+// const b= 3;
+
+// if (c === 10 && e === 10) {
+//     console.log("sunt egale")
+// } else {
+//     console.log("nus egale")
+// }
+// if (a > b) {
+//     console.log("tfue")
+// } else {
+//     console.log("false")
+// }
+
+// if (a > 10) {
+//     console.log("incorect")
+// } else if(a === "2") {
+//     console.log("sunt egale")
+// }
+// else{
+//     console.log("errore")
+// }
+
+// let fruit = "apple"
+
+// switch (fruit) {
+//     case "orange":
+//         console.log("Orange $0.59")
+        
+//         break;
+//     case "apple":
+//         console.log("apple $1.59")
+            
+//         break; 
+//     case "banana":
+//         console.log("banana $2.59")
+            
+//         break;
+//     default:
+//         break;
+// }
+
+// let i = 1
+
+// while (i => 5) {
+//     console.log(i)
+//     i += 1
+// }
+
+// do {
+//     console.log(i);
+//     i += 1;
+// }while(i <= 5)
+// for (let i = 0; 1 <= 5; i++) {
+//     console.log(1)
+// }
+// let array = [
+//     "patru"
+//     "unu"
+//     "doi"
+//     "trei"
+//     "patru"
+//     "cinci"
+// ];
+// array.forEach(el =>{
+//     if (el.includes("patru")){
+//         console.log(el)
+//     }
+// })
+
+const first_number = prompt("first_number")
+const operator = prompt("mathematical operator")
+const second_number = prompt(second_number)
+
+
+if (operator === "+"){
+    console.log(Number(first_number) + Number(second_number))
+}else if(operator === "-"){
+console.log(Number(first_number) + Number(second_number))
+}else if(operator === "*"){ 
+console.log(Number(first_number) + Number(second_number))
+}else if(operator === "/"){
+console.log(Number(first_number) + Number(second_number))
+}else if(operator === "**");
